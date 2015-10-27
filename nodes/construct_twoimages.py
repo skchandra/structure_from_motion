c56b7a740191c4b369ec80764a27c8d134cf9b52
Kiki <shivali.chandra@students.olin.edu>
2015-10-26 22:00:28 -0400

    Commented, final version of code

--- nodes/construct_twoimages.py
+++ nodes/construct_twoimages.py
@@ -1,138 +1,38 @@
 #!/usr/bin/env python
 import rospy
-from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image
 
-import cv2, numpy as np, math, matplotlib.pyplot as plt
+import cv2
 from cv_bridge import CvBridge
-
-from show_depth import *
 
 from sfm import *
 
-# D = np.array( [0.07151361939824905, -0.19481871818164997, 0.006885482396599795, -0.0065266433799991965, 0.0])
-# K = np.array( [[634.1625796919535, 0.0, 302.8222037975313], [0.0, 633.0839114382693, 253.7043901565232], [0.0, 0.0, 1.0]])
-
 class Reconstruct3D:
 	def __init__(self):
+		"""Initialize subscriptions to topics, such as the raw video"""
+
 		rospy.init_node('construct_images')
 		rospy.Subscriber('/camera/image_raw', Image, self.image_callback)
-		rospy.Subscriber('/odom', Odometry, self.odom_callback)
 
-		#self.cap = cv2.VideoCapture('/camera/image_raw')
-
+		# Raw images from the Neato
 		self.image = None
 		self.bridge = CvBridge()
 
+		# Stores 2 images when the 'c' key is pressed
 		self.image1 = None
 		self.image2 = None
-
-		self.correspondences = ([], [])
 
 		self.calculated_flow = False
 
 	def image_callback(self, msg):
+		"""Callback function to continuously set self.image"""
+
 		self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
-
-	def odom_callback(self, msg):
-		pass
-
-	def optical_flow(self):
-		"""sift = cv2.SIFT()
-
-		old_image = cv2.cvtColor(self.image1, cv2.COLOR_BGR2GRAY)
-		new_image = cv2.cvtColor(self.image2, cv2.COLOR_BGR2GRAY)
-
-		pts0, des0 = sift.detectAndCompute(old_image, None)
-		pts1, des1 = sift.detectAndCompute(new_image, None)
-
-		bf = cv2.BFMatcher()
-		matches = bf.knnMatch(des0, des1, k=2)
-
-		good = []
-		for m,n in matches:
-		    if m.distance < 50:
-		        good.append([m])
-
-		# Apply ratio test
-		for m,n in matches:
-		    if m.distance < 0.75*n.distance:
-		    	self.correspondences[0].append((m.queryIdx, m.trainIdx))
-		    	self.correspondences[1].append((n.queryIdx, n.trainIdx))"""
-
-		# old_image = cv2.cvtColor(self.image1, cv2.COLOR_BGR2GRAY)
-		# new_image = cv2.cvtColor(self.image2, cv2.COLOR_BGR2GRAY)
-		# # # cv2.imshow('distorted', old_image)
-		# # # old_image = cv2.undistort(old_image,K,D)
-		# # # cv2.imshow('undistort', old_image)
-		# # # new_image = cv2.undistort(new_image,K,D)
-		# flow = cv2.calcOpticalFlowFarneback(old_image, new_image, 0.5, 3, 12, 3, 5, 1.2, 0)
-		
-		# # create array of points
-		# for y in range(0, old_image.shape[0], 6):
-		# 	for x in range(0, old_image.shape[1], 6):
-		# 		if abs(flow[y][x][0] > 0.1) and abs(flow[y][x][1] > 0.1):
-		# 			self.correspondences[0].append((y,x))
-		# 			self.correspondences[1].append((y + flow[y][x][0],
-		# 											x + flow[y][x][1]))
-		construct(self.image1, self.image2)
-		
-		# sift = cv2.SIFT()
-
-		# pts0 = sift.detect(self.image1, None)
-		# for i in range(len(pts0)):
-		# 	pts0[i] = [[pts0[i].pt[0], pts0[i].pt[1]]]
-		# pts0 = np.array(pts0, np.float32)
-
-		# pts1 = np.zeros(pts0.shape)
-		# for i in range(len(pts0)):
-		# 	y = int(pts0[i][0][1] + 0.5)
-		# 	x = int(pts0[i][0][0] + 0.5)
-		# 	dy = flow[y][x][0]
-		# 	dx = flow[y][x][1]
-		# 	self.correspondences[0].append((y,x))
-		# 	self.correspondences[1].append((y+dy, x+dx))
-
-
-		# flow = cv2.calcOpticalFlowFarneback(old_image, new_image, 0.5, 3, 15, 3, 5, 1.2, 0)
-
-		# for y in range(old_image.shape[0]):
-		# 	for x in range(old_image.shape[1]):
-		# 		if math.sqrt( flow[y][x][0]**2 + flow[y][x][1]**2 > 70 ):
-		# 			self.correspondences[0].append((y,x))
-		# 			self.correspondences[1].append((y + flow[y][x][0],
-		# 											x + flow[y][x][1]))
-
-		# lk_params = dict( winSize = (10,10),
-		# 				  maxLevel = 10,
-		# 				  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03) )
-
-		# color = [255,0,0]
-
-		# Keypoint matching
-		# old_image = cv2.cvtColor(self.image1, cv2.COLOR_BGR2GRAY)
-		# new_image = cv2.cvtColor(self.image2, cv2.COLOR_BGR2GRAY)
-		# sift = cv2.SIFT()
-
-		# pts0 = sift.detect(self.image1, None)
-		# for i in range(len(pts0)):
-		# 	pts0[i] = [[pts0[i].pt[0], pts0[i].pt[1]]]
-		# pts0 = np.array(pts0, np.float32)
-		# pts1, st, err = cv2.calcOpticalFlowPyrLK(old_image, new_image, pts0, None, **lk_params)
-
-		# good_new = pts1[st==1]
-		# good_old = pts0[st==1]
-
-		# for i,(new,old) in enumerate(zip(good_new, good_old)):
-		# 	a,b = new.ravel()
-		# 	c,d = old.ravel()
-		# 	self.correspondences[0].append((c,d))
-		# 	self.correspondences[1].append((a,b))
-		# 	# cv2.line(self.image2, (a,b), (c,d), color, 2)
-		# 	# cv2.circle(self.image2, (a,b), 5, color, -1)
-		
 	def run(self):
+		"""Runs the program until q pressed or program stopped. In this process the two images are saved 
+		and the function to calculate the optical flow and depth is called."""
+
 		r = rospy.Rate(10)
 
 		while not rospy.is_shutdown():
@@ -140,18 +40,14 @@ class Reconstruct3D:
 			if self.image != None:
 				if self.image2 == None:
 					cv2.imshow('image_raw', self.image)
-					if cv2.waitKey(1) & 0xFF == ord('c'):
+					if cv2.waitKey(1) & 0xFF == ord('c'):	#if 'c' key is pressed take a pic from Neato camera
 						if self.image1 == None:
 							self.image1 = self.image
 						elif self.image2 == None:
 							self.image2 = self.image
-				elif not self.calculated_flow:
-					self.optical_flow()
+				elif not self.calculated_flow:	#if the two images are captured and optical flow hasn't been calculated, set to True and run calculations
 					self.calculated_flow = True
-					# show_depth(self.image1, self.image2, self.correspondences)
-
-					#cv2.imshow('before flow', self.image1)
-					#cv2.imshow('optical flow', self.image2)
+					construct(self.image1, self.image2) 	#call function from sfm.py file 
 
 			if cv2.waitKey(30) & 0xFF == ord('q'):
 				cv2.destroyAllWindows()
--- nodes/sfm.py
+++ nodes/sfm.py
@@ -1,13 +1,21 @@
 import cv2
 import numpy as np 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-D = np.array( [0.12969982463039115, -0.3662323365747535, -0.0047588179431673535, 0.0032280633422334627, 0.0])
-K = np.array( [[672.9140718714626, 0.0, 317.4277026418419], [0.0, 670.9812424489073, 216.90441559892045], [0.0, 0.0, 1.0]])
+#camera matrix and distortion values to use to undistort images 
+D = np.array( [0.07449795106852891, -0.26800080727246195, 0.00010699178813798149, -0.0016215045904716067, 0.0])
+K = np.array( [[649.0759367719918, 0.0, 313.97037517958785], [0.0, 645.5178014943875, 230.33935085177768], [0.0, 0.0, 1.0]])
 W = np.array([[0.0, -1.0, 0.0],
 			  [1.0, 0.0, 0.0],
 			  [0.0, 0.0, 1.0]])
 
 def draw_flow(im,flow,step=16):
+	"""Function to visualize optical flow on the 2nd image in order to see change. 
+	Input: 2nd image, optical flow array, step
+	Output: 2nd image with optical flow lines.
+	source: http://stackoverflow.com/questions/13685771/opencv-python-calcopticalflowfarneback"""
+
     h,w = im.shape[:2]
     y,x = np.mgrid[step/2:h:step,step/2:w:step].reshape(2,-1)
     fx,fy = flow[y,x].T
@@ -24,30 +32,49 @@ def draw_flow(im,flow,step=16):
     return vis
 
 def test_triangulation(P,pcloud):
-	P4x4 = np.eye(4)
+	"""Calculates most likely projection matrix of the objects. 
+	Input: List of possible projection matrices, point cloud
+	Output: The average of all the z coordinates in the point cloud (if they are greater than 0)
+	source: show_depth.py (included in Git repo) from Paul Ruvolo"""
+
+	P4x4 = np.eye(4)	
 	P4x4[0:3,:] = P
 	pcloud_3d = pcloud[:,0:3]
 	projected = cv2.perspectiveTransform(np.array([pcloud_3d]),P4x4)
 	return np.mean(projected[0,:,2]>0.0)
 
 def construct(im1, im2):
+	"""Function to calculate optical flow between two images and extrapolate x, y, and z coordinates of each pixel
+	that has moved above a certain threshold. Also produces a 3d plot of the object(s).
+	Input: 2 images of the same objects
+	Output: none """
+
 	old_image = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
 	new_image = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+	#calculates flow and calls function to draw the flow vectors on the image
 	flow = cv2.calcOpticalFlowFarneback(old_image, new_image, 0.5, 3, 12, 3, 5, 1.2, 0)
 	cv2.imshow('Optical flow',draw_flow(new_image,flow))
-	correspondences = ([], [])
 
-	for y in range(0, old_image.shape[0], 1):
-			for x in range(0, old_image.shape[1], 1):
+	correspondences = ([], [])	#array of matched keypoints between two images
+
+	#iterate through first image and get optical flow for every pixel in every 3rd row and 3rd column, if it is larger than threshold
+	for y in range(0, old_image.shape[0], 3):
+			for x in range(0, old_image.shape[1], 3):
 				if abs(flow[y][x][0]) > 0.3 and abs(flow[y][x][1]) > 0.3:
 					correspondences[0].append((y,x))
 					correspondences[1].append((y + flow[y][x][0],
 													x + flow[y][x][1]))
 
+	#source of following code: show_depth.py from Paul Ruvolo
 	im1_pts = np.zeros((len(correspondences[0]),2))
 	im2_pts = np.zeros((len(correspondences[1]),2))
 
+	#creating a visualization of the keypoints on the first image
 	im = np.array(np.hstack((im1,im2)))
+	for i in range(len(correspondences[0])):
+		cv2.circle(im, (int(correspondences[0][i][1]), int(correspondences[0][i][0])),1,(0,255,255),1)
+	cv2.imshow("keypoints",im)
 
 	# plot the points
 	for i in range(len(im1_pts)):
@@ -56,28 +83,23 @@ def construct(im1, im2):
 		im2_pts[i,0] = correspondences[1][i][0]
 		im2_pts[i,1] = correspondences[1][i][1]
 
-		# cv2.circle(im,(int(im1_pts[i,0]),int(im1_pts[i,1])),2,(255,0,0),2)
-		# cv2.circle(im,(int(im2_pts[i,0]+im1.shape[1]),int(im2_pts[i,1])),2,(255,0,0),2)
 
 	im1_pts_augmented = np.zeros((1,im1_pts.shape[0],im1_pts.shape[1]))
 	im1_pts_augmented[0,:,:] = im1_pts
 	im2_pts_augmented = np.zeros((1,im2_pts.shape[0],im2_pts.shape[1]))
 	im2_pts_augmented[0,:,:] = im2_pts
 
-	#im1_pts_ud = im1_pts_augmented
-	#im2_pts_ud = im2_pts_augmented
-	print im1_pts_augmented.shape
-	print im2_pts_augmented.shape
 	im1_pts_ud = cv2.undistortPoints(im1_pts_augmented,K,D)
 	im2_pts_ud = cv2.undistortPoints(im2_pts_augmented,K,D)
-
+	#end sourced code
 	
+	#calculate essential matrix
 	F, mask = cv2.findFundamentalMat(im1_pts_ud, im2_pts_ud, cv2.FM_RANSAC, 3, 0.99)
 	E = K.T * F * K
 	W_inv = np.linalg.inv(W)
 
+	# get rotational/translational components from E
 	U, S, V = np.linalg.svd(E)
-
 	R1 = U * W * V.T
 	if np.linalg.det(R1)+1 < 10**-8:
 		F = -F
@@ -89,7 +111,9 @@ def construct(im1, im2):
 	R2 = U * W_inv * V.T
 	T2 = [-i[2] for i in U]
 
-	P1 = np.eye(3, 4, dtype=np.float64)
+	P1 = np.eye(3, 4, dtype=np.float64)   #identity projection matrix
+
+	#all possibilities for P2
 	P2 = np.array([[R1[0][0], R1[0][1], R1[0][2], T1[0]],
 					[R1[1][0], R1[1][1], R1[1][2], T1[1]],
 					[R1[2][0], R1[2][1], R1[2][2], T1[2]]])
@@ -100,82 +124,137 @@ def construct(im1, im2):
 	P2_possibilities.append(np.column_stack((R2,T1)))
 	P2_possibilities.append(np.column_stack((R2,T2)))
 
+	#triangulate points for every possible projection matrix
 	cloud = []
 	for i in P2_possibilities:
 		cloud.append(cv2.triangulatePoints(P1, i, im1_pts_ud, im2_pts_ud))
 
+	#finding most likely projection matrix for each possibility. 
+	#source: show_depth.py from Paul Ruvolo
 	infront_of_camera = []
 	for i in range(len(P2_possibilities)):
 		infront_of_camera.append(test_triangulation(P1, cloud[i]) + test_triangulation(P2_possibilities[i], cloud[i]))
 
 	cloud = cloud[np.argmax(infront_of_camera)]
+	#end sourced code
 
 	output = np.array(cloud)
-	output = output.T
+	output = output.T 	#transpose to change format of output
 	x = []
 	y = []
 	z = []
+
+	#normalizing the x, y, z coordinates
 	for i in output:
+		print i
+		i[0] = i[0]/i[3]
 		x.append(i[0])
+		i[1] = i[1]/i[3]
 		y.append(i[1])
+		i[2] = i[2]/i[3]
 		z.append(i[2])
-
-	for i in range(len(correspondences[0])):
-		cv2.circle(im, (int(correspondences[0][i][1]), int(correspondences[0][i][0])),1,(0,255,0),1)
-
-	cv2.imshow("MYWIN",im)
-	# for i in output:
-	# 	print i
-	# 	i[0] = i[0]/i[3]
-	# 	x.append(i[0])
-	# 	i[1] = i[1]/i[3]
-	# 	y.append(i[1])
-	# 	i[2] = i[2]/i[3]
-	# 	z.append(i[2])
-	# 	i[3] = i[3]/i[3]
-
-	# print output
+		i[3] = i[3]/i[3]
 	
-	import matplotlib.pyplot as plt
-	from mpl_toolkits.mplot3d import Axes3D
+	#plotting the object in a scatter plot
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
-	ax.scatter(x, y, z)
+	ax.scatter(x, y, z, c='b')
 	plt.show()
 
-	# F = np.linalg.inv(K.T).dot(E).dot(np.linalg.inv(K))
-	# U, Sigma, V = np.linalg.svd(E)
 
-	# R1 = U.dot(W).dot(V)
-	# R2 = U.dot(W.T).dot(V)
+# old function with some previous calculations that we tried
 
-	# if np.linalg.det(R1)+1.0 < 10**-8:
-	# 	# flip sign of E and recompute everything
-	# 	E = -E
-	# 	F = np.linalg.inv(K.T).dot(E).dot(np.linalg.inv(K))
-	# 	U, Sigma, V = np.linalg.svd(E)
 
-	# 	R1 = U.dot(W).dot(V)
-	# 	R2 = U.dot(W.T).dot(V)
+	#	def optical_flow(self):
+		# sift = cv2.SIFT()
 
-	# t1 = U[:,2]
-	# t2 = -U[:,2]
+		# old_image = cv2.cvtColor(self.image1, cv2.COLOR_BGR2GRAY)
+		# new_image = cv2.cvtColor(self.image2, cv2.COLOR_BGR2GRAY)
 
-	# P = np.array([[1.0,	0.0, 0.0, 0.0],
-	# 			  [0.0,	1.0, 0.0, 0.0],
-	# 			  [0.0,	0.0, 1.0, 0.0]]);
-	# P1_possibilities = []
-	# P1_possibilities.append(np.column_stack((R1,t1)))
-	# P1_possibilities.append(np.column_stack((R1,t2)))
-	# P1_possibilities.append(np.column_stack((R2,t1)))
-	# P1_possibilities.append(np.column_stack((R2,t2)))
+		# pts0, des0 = sift.detectAndCompute(old_image, None)
+		# pts1, des1 = sift.detectAndCompute(new_image, None)
 
-	# pclouds = []
-	# for P1 in P1_possibilities:
-	# 	pclouds.append(triangulate_points(im1_pts_ud_fixed, im2_pts_ud_fixed, P, P1))
+		# bf = cv2.BFMatcher()
+		# matches = bf.knnMatch(des0, des1, k=2)
 
-	# infront_of_camera = []
-	# for i in range(len(P1_possibilities)):
-	# 	infront_of_camera.append(test_triangulation(P,pclouds[i])+test_triangulation(P1_possibilities[i],pclouds[i]))
+		# good = []
+		# for m,n in matches:
+		#     if m.distance < 50:
+		#         good.append([m])
 
-	# best_pcloud = pclouds[np.argmax(infront_of_camera)]
+		# # Apply ratio test
+		# for m,n in matches:
+		#     if m.distance < 0.75*n.distance:
+		#     	self.correspondences[0].append((m.queryIdx, m.trainIdx))
+		#     	self.correspondences[1].append((n.queryIdx, n.trainIdx))
+
+		# old_image = cv2.cvtColor(self.image1, cv2.COLOR_BGR2GRAY)
+		# new_image = cv2.cvtColor(self.image2, cv2.COLOR_BGR2GRAY)
+		# # # cv2.imshow('distorted', old_image)
+		# # # old_image = cv2.undistort(old_image,K,D)
+		# # # cv2.imshow('undistort', old_image)
+		# # # new_image = cv2.undistort(new_image,K,D)
+		# flow = cv2.calcOpticalFlowFarneback(old_image, new_image, 0.5, 3, 12, 3, 5, 1.2, 0)
+		
+		# # create array of points
+		# for y in range(0, old_image.shape[0], 6):
+		# 	for x in range(0, old_image.shape[1], 6):
+		# 		if abs(flow[y][x][0] > 0.1) and abs(flow[y][x][1] > 0.1):
+		# 			self.correspondences[0].append((y,x))
+		# 			self.correspondences[1].append((y + flow[y][x][0],
+		# 											x + flow[y][x][1]))
+		#construct(self.image1, self.image2)
+		
+		# sift = cv2.SIFT()
+
+		# pts0 = sift.detect(self.image1, None)
+		# for i in range(len(pts0)):
+		# 	pts0[i] = [[pts0[i].pt[0], pts0[i].pt[1]]]
+		# pts0 = np.array(pts0, np.float32)
+
+		# pts1 = np.zeros(pts0.shape)
+		# for i in range(len(pts0)):
+		# 	y = int(pts0[i][0][1] + 0.5)
+		# 	x = int(pts0[i][0][0] + 0.5)
+		# 	dy = flow[y][x][0]
+		# 	dx = flow[y][x][1]
+		# 	self.correspondences[0].append((y,x))
+		# 	self.correspondences[1].append((y+dy, x+dx))
+
+
+		# flow = cv2.calcOpticalFlowFarneback(old_image, new_image, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+		# for y in range(old_image.shape[0]):
+		# 	for x in range(old_image.shape[1]):
+		# 		if math.sqrt( flow[y][x][0]**2 + flow[y][x][1]**2 > 70 ):
+		# 			self.correspondences[0].append((y,x))
+		# 			self.correspondences[1].append((y + flow[y][x][0],
+		# 											x + flow[y][x][1]))
+
+		# lk_params = dict( winSize = (10,10),
+		# 				  maxLevel = 10,
+		# 				  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03) )
+
+		# color = [255,0,0]
+
+		# Keypoint matching
+		# old_image = cv2.cvtColor(self.image1, cv2.COLOR_BGR2GRAY)
+		# new_image = cv2.cvtColor(self.image2, cv2.COLOR_BGR2GRAY)
+		# sift = cv2.SIFT()
+
+		# pts0 = sift.detect(self.image1, None)
+		# for i in range(len(pts0)):
+		# 	pts0[i] = [[pts0[i].pt[0], pts0[i].pt[1]]]
+		# pts0 = np.array(pts0, np.float32)
+		# pts1, st, err = cv2.calcOpticalFlowPyrLK(old_image, new_image, pts0, None, **lk_params)
+
+		# good_new = pts1[st==1]
+		# good_old = pts0[st==1]
+
+		# for i,(new,old) in enumerate(zip(good_new, good_old)):
+		# 	a,b = new.ravel()
+		# 	c,d = old.ravel()
+		# 	self.correspondences[0].append((c,d))
+		# 	self.correspondences[1].append((a,b))
+		# 	# cv2.line(self.image2, (a,b), (c,d), color, 2)
+		# 	# cv2.circle(self.image2, (a,b), 5, color, -1)
